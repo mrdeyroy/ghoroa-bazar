8ac27d21d1c02a322c3aa7177b702d0ad4f74089
mrdeyroy <shibamdeyroy02@gmail.com>
2026-03-25 23:26:59 +0530

    new account create issue

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,11 +2,24 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.log("SMTP ERROR:", err);
+  } else {
+    console.log("SMTP READY");
+  }
 });
 
 const sendEmail = async (to, subject, html) => {
@@ -21,7 +34,7 @@ const sendEmail = async (to, subject, html) => {
     await transporter.sendMail(mailOptions);
     console.log("Email sent successfully to:", to);
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error("FULL EMAIL ERROR:", error);
     throw new Error("Failed to send email");
   }
 };
